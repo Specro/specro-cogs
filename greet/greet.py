1067d931eb41d067ec50a4d3da5cb61850632e71
Liudas Dzisevicius <liudas.dzisevicius@gmail.com>
2016-10-20 22:42:35 +0300

    Add greet cog

---
+++ greet/greet.py
@@ -0,0 +1,20 @@
+import discord
+from discord.ext import commands
+from beeprint import pp
+
+class Greet:
+    """Greets people when they login!"""
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    async def on_member_join(self, member):
+        server = member.server
+        await self.bot.send_message(server, "Welcome to " + server.name + " " + member.mention + "! Thanks for joining and enjoy your stay!")
+
+    async def on_member_remove(self, member):
+        server = member.server
+        await self.bot.send_message(server, member.mention + " has left the Discord server.")
+
+def setup(bot):
+    bot.add_cog(Greet(bot))
